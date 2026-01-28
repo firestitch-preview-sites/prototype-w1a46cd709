d2ca469b0fd50f6e0ebcd73c85021d5ec5b12463
Specify Prototype <prototype@specify.app>
2026-01-28 15:03:33 -0500

    Initial Angular boilerplate

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `<h1>Preview Site</h1>`,
+  styles: [`h1 { margin: 20px; }`],
+})
+export class AppComponent {
+}
